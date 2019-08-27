db0dd7025acf9497cb9cbfa19d9996d5153e0c5b
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2019-08-27 13:04:41 -0700

    Added appointment functionality

--- appointments/index.js
+++ appointments/index.js
@@ -44,6 +44,134 @@ router.get("/", (req, res) => {
   }
 });
 
+router.put("/:id", (req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json({
+      message:
+        "You must provide an 'authorization' header with a valid token in order to access this resource."
+    });
+  } else {
+    jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET,
+      (err, decodedToken) => {
+        if (err) {
+          return res
+            .status(401)
+            .json({ message: "Unauthorized: Invalid token" });
+        } else {
+          apptDb
+            .findById(req.params.id)
+            .then(appts => {
+              const appt = appts[0];
+
+              if (decodedToken.id !== appt.business_id) {
+                return res.status(401).json({
+                  message:
+                    "Unauthorized: You may not update appointments that do not belong to you."
+                });
+              } else {
+                if (appt) {
+                  const morphed = {
+                    ...appt,
+                    ...req.body
+                  };
+                  const newAppt = {
+                    id: appt.id,
+                    quantity: morphed.quantity,
+                    status: morphed.status,
+                    time: morphed.time,
+                    type: morphed.type,
+                    business_id: morphed.business_id,
+                    volunteer_id: morphed.volunteer_id
+                  };
+                  apptDb
+                    .update(req.params.id, newAppt)
+                    .then(entries => {
+                      return res.status(202).json(newAppt);
+                    })
+                    .catch(err => {
+                      console.log(err);
+                      return res.status(500).json({
+                        message:
+                          "Internal failure when updating the specified appointment."
+                      });
+                    });
+                } else {
+                  return res.status(404).json({
+                    message:
+                      "The request appointment does not exist. Please provide a valid ID."
+                  });
+                }
+              }
+            })
+            .catch(err => {
+              console.log(err);
+              return res.status(500).json({
+                message: "Failed in retrieving the specified appointment."
+              });
+            });
+        }
+      }
+    );
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json({
+      message:
+        "You must provide an 'authorization' header with a valid token in order to access this resource."
+    });
+  } else {
+    jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET,
+      (err, decodedToken) => {
+        if (err) {
+          return res
+            .status(401)
+            .json({ message: "Unauthorized: Invalid token" });
+        } else {
+          apptDb
+            .findById(req.params.id)
+            .then(appts => {
+              const appt = appts[0];
+
+              if (decodedToken.id !== appt.business_id) {
+                return res.status(401).json({
+                  message:
+                    "Unauthorized: You may not delete appointments that do not belong to you."
+                });
+              } else {
+                apptDb
+                  .deleteById(req.params.id)
+                  .then(affected => {
+                    res.status(200).json(appt);
+                  })
+                  .catch(err => {
+                    console.log(err);
+                    return res
+                      .status(500)
+                      .json({
+                        message:
+                          "Appointment deletion failed due to an internal error."
+                      });
+                  });
+              }
+            })
+            .catch(err => {
+              console.log(err);
+              return res.status(500).json({
+                message: "Failed in retrieving the specified appointment."
+              });
+            });
+        }
+      }
+    );
+  }
+});
+
 router.post("/", (req, res) => {
   if (!req.headers.authorization) {
     res.status(401).json({
@@ -79,28 +207,32 @@ router.post("/", (req, res) => {
               !status ||
               !status.trim()
             ) {
-              return res
-                .status(400)
-                .json({
-                  message:
-                    "Please provide 'time', 'quantity', 'type' and 'status' fields to create a new appointment."
-                });
+              return res.status(400).json({
+                message:
+                  "Please provide 'time', 'quantity', 'type' and 'status' fields to create a new appointment."
+              });
             } else {
-              const newAppt = 
-              {
-                time, quantity, type, status,
+              const newAppt = {
+                time,
+                quantity,
+                type,
+                status,
                 business_id: decodedToken.id
               };
 
-              apptDb.insert(newAppt)
-              .then(id => {
-                newAppt.id = id[0];
-                res.status(201).json(newAppt);
-              })
-              .catch(err => {
-                console.log(err);
-                return res.status(500).json({message: "Failed to create appointment due to an internal error."});
-              });
+              apptDb
+                .insert(newAppt)
+                .then(id => {
+                  newAppt.id = id[0];
+                  res.status(201).json(newAppt);
+                })
+                .catch(err => {
+                  console.log(err);
+                  return res.status(500).json({
+                    message:
+                      "Failed to create appointment due to an internal error."
+                  });
+                });
             }
           }
         }
